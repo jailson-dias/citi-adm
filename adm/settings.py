5eb0172d15a9cb776dce55a6e4d974a7a0c6f9c2
Jailson Dias <jcd2@cin.ufpe.br>
2019-06-01 09:28:58 -0300

    first commit

--- adm/settings.py
+++ adm/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
